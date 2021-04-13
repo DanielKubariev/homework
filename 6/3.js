94743815765c38cbfe21d16246c70f5aaf7b7eb4
Daniel Kubariev <danielkubariev@gmail.com>
2021-04-13 20:08:15 +0300

    Finished HW6

--- 6/3.js
+++ 6/3.js
@@ -1,22 +1,24 @@
 // 3. Принимая массив объектов и случайную строку. У объектов может быть ключ: «title» с разными значениями.
 // Создайте алгоритм, который фильтрует массив, заданный как первый параметр, и возвращает массив объектов,
 // которые содержат в своих заголовках заданную строку в качестве второго параметра (без учета регистра).
-// function findTitle(array, ‘string’) {
-//   return [{title has this substring}]
-//   }
-  
-//   findTitle (arr, str) {
-//   ...
-//   }
-  
-//   let arr = [{
-//   title: 'Some title1'
-//   }, {
-//   title: 'I like JS'
-//   }, {
-//   user: 'This obj doesn\’t have key title js'
-//   }, {
-//   title: 'Js - is the best!'
-//   }];
-//   findTitle(arr, 'js'); // return [{ title: 'I like JS'}, { title: 'Js - is the best!' }]
-  
+var arr = [
+  {title: 'Some title'}, 
+  {title: 'I like JS'}, 
+  {nonTitle: 'This obj doesn’t have key title js'},
+  {title: 'Js - is the best!'}
+];
+
+function findStr(arr,str){
+  var newArr =[];
+  for(var i = 0; i < arr.length; i++){
+    if(String(arr[i].title).toLowerCase().includes(str.toLowerCase()) ){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+
+
+
+console.log(findStr(arr, 'js')); 
--- 6/4.js
+++ 6/4.js
@@ -6,4 +6,17 @@
 // }
 // countCharacters(‘sparrow’) // should return {s: 1, p: 1, a: 1, r: 2, o: 1, w: 1}
 // countCharacters(‘aabcddeffge’) // should return {a: 2, b: 1, c: 1, d: 2, e: 2, f: 2, g: 1}
-// countCharacters(‘a 2ab !d’) // should return {a: 2, b:1, d:1, 2:1}
+// countCharacters(‘a 2ab !d’) // should return {a: 2, b:1, d:1, 2:1} //charCodeAt >= 65 <=90
+
+function countCharacters(str){
+	var obj = {};
+	for(var i = 0; i < str.length; i++){
+		if(obj.hasOwnProperty(str[i])){
+			obj[str[i]]++;
+		} else if(str[i].toUpperCase().charCodeAt(0) >= 65 && str[i].toUpperCase().charCodeAt(0) <= 90){
+			obj[str[i]] = 1;
+		}
+	}
+	 return obj;
+}
+console.log(countCharacters('a 2ab !d'));
